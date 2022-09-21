d5929012da44fdfc9e1e2ec69943f7c446960e1e
Onee12 <arielmmartz@gmail.com>
2022-09-21 16:55:50 -0300

    Updates

---
+++ precache-manifest.623fd1a56b4ccffda6b34489ee544e0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4478e4b4a91d26611fb7",
+    "url": "/weatherReact/static/css/main.a8db2b6d.chunk.css"
+  },
+  {
+    "revision": "4478e4b4a91d26611fb7",
+    "url": "/weatherReact/static/js/main.4478e4b4.chunk.js"
+  },
+  {
+    "revision": "14b060c7915162ae1f5f",
+    "url": "/weatherReact/static/js/1.14b060c7.chunk.js"
+  },
+  {
+    "revision": "7ff0fdc563cdc50f3104",
+    "url": "/weatherReact/static/js/2.7ff0fdc5.chunk.js"
+  },
+  {
+    "revision": "215c42831dace49ca53f",
+    "url": "/weatherReact/static/js/runtime~main.215c4283.js"
+  },
+  {
+    "revision": "bc25f7aa113ff546cde96f3ae7b97f23",
+    "url": "/weatherReact/index.html"
+  }
+];
